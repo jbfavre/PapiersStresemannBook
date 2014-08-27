3ca73664574d99511c9b02ff40dd33d6ae851012
Jean Baptiste Favre <github@jbfavre.org>
2014-08-27 18:11:08 +0200

    Update utilities

--- utilities/bookscanner_cat_them_all.sh
+++ utilities/bookscanner_cat_them_all.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-output=full_book.txt
+if [ $# -gt 1 ];
+then
+  scope="part"
+  start=$1
+  stop=$2
+elif [ $# -eq 0 ];
+then
+  scope="all"
+fi
 
-sed ':a;N;$!ba;
-     s/-\n//g;
-     s/—\n//g;
-     s/ \n/\n/g;
-     s/\n\{2,\}/double_new_line/g;
-     s/\n/ /g;
-     s/double_new_line/\n\n/g;
-     ' bookocr/*.txt > ${output}
-
-sed -i 's/ﬁ/fi/g' ${output}
-sed -i 's/ﬂ/fl/g' ${output}
+for indice in $(seq -f "%05g" ${start} ${stop});
+do
+  if [ -f "bookocr/i${indice}.tif.txt" ];
+  then
+    sed ':a;N;$!ba;
+         s/-\n//g;
+         s/—\n//g;
+         s/ \n/\n/g;
+         s/\n\{2,\}/double_new_line/g;
+         s/\n/ /g;
+         s/double_new_line/\n\n/g;
+         ' bookocr/i${indice}.tif.txt
+  fi
+done
